7417b03961dfa7405e5d3080de5d26d9691a544a
Michael Mudge <michaeltmudge@gmail.com>
2018-09-06 09:32:11 +0200

    Dev tool initial commit

---
+++ lib/mudge_view_tool.rb
@@ -0,0 +1,6 @@
+require "mudge_view_tool/version"
+require "mudge_view_tool/renderer"
+
+module MudgeViewTool
+  # Your code goes here...
+end
---
+++ lib/mudge_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MudgeViewTool
+  VERSION = "0.1.0"
+end
